2cf6a5f2c5972f5d813ac0df2365709f5390fc0f
itesanovic <itesanovic@execom.eu>
2020-05-11 08:23:23 +0200

    Removing commented code. Formatting code.

--- hacker-news-app/src/app/stories/state/story.effect.ts
+++ hacker-news-app/src/app/stories/state/story.effect.ts
@@ -1,23 +1,26 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import { StoriesService } from '../stories.service';
-import { mergeMap, catchError, map, flatMap, zip, switchMap } from 'rxjs/operators';
+import { mergeMap, catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
 import * as fromStoryActions from './story.actions';
 
 @Injectable()
 export class StoryEffect {
-    constructor(private actions$: Actions,
-        private storiesService: StoriesService) { }
+  constructor(
+    private actions$: Actions,
+    private storiesService: StoriesService
+  ) {}
 
-    stories$ = createEffect(() => this.actions$.pipe(
-        ofType(fromStoryActions.StoryActionTypes.Load),
-        mergeMap((payload) => this.storiesService.loadTopStories(0).pipe(
-            map(data => fromStoryActions.LoadSuccess({ stories: data })),
-            catchError(error => of(fromStoryActions.LoadFail({ error: error })))
-        ))));
-
-    // stories$ = this.actions$.pipe(
-    //     ofType(fromStoryActions.StoryActionTypes.Load),
-    //     mergeMap(() => from(this.storiesService.loadTopStories(0))));
+  stories$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromStoryActions.StoryActionTypes.Load),
+      mergeMap((payload) =>
+        this.storiesService.loadTopStories(0).pipe(
+          map((data) => fromStoryActions.LoadSuccess({ stories: data })),
+          catchError((error) => of(fromStoryActions.LoadFail({ error: error })))
+        )
+      )
+    )
+  );
 }
